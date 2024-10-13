cdb066f62a1922838302a29ccb080345ebf77139
Jonathan Ryan <jonathan071106@gmail.com>
2024-10-13 20:32:13 +0700

    added comments

--- src/binary_search.py
+++ src/binary_search.py
@@ -3,17 +3,25 @@
 def binary_seach(val: int, ordered_list: list[int]) -> int:
     index: int = 0
 
+    # Loop until length of ordered list is 1
     while len(ordered_list) > 1:
+        # Get middle index
         mid: int = len(ordered_list) // 2
+
+        # If val is less than the middle value search in the left half
         if val < ordered_list[mid]:
             ordered_list = ordered_list[:mid]
+        # If val is more than the middle value search in the right half
         elif val > ordered_list[mid]:
             index += mid
             ordered_list = ordered_list[mid:]
+        # If val is equal to the middle value
         else:
             index += mid
             break
+    # Execute once the length of ordered list is 1
     else:
+        # Value doesn't exist
         if ordered_list[0] != val:
             return -1
     
