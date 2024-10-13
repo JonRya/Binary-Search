cea9371cd4046490900c147622256ef5877212e0
Jonathan Ryan <jonathan071106@gmail.com>
2024-10-13 20:20:04 +0700

    use while loop instead of recursion

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,20 +1,28 @@
 # Returns the index of the value if the value exists
 # Otherwise returns -1
 def binary_seach(val: int, ordered_list: list[int]) -> int:
-    mid = len(ordered_list) // 2
+    index: int = 0
 
-    if val < ordered_list[mid]:
-        mid = binary_seach(val, ordered_list[:mid])
-    elif val > ordered_list[mid]:
-        mid = mid + binary_seach(val, ordered_list[mid:])
+    while len(ordered_list) > 1:
+        mid: int = len(ordered_list) // 2
+        if val < ordered_list[mid]:
+            ordered_list = ordered_list[:mid]
+            # mid = binary_seach(val, ordered_list[:mid])
+        elif val > ordered_list[mid]:
+            index += mid
+            ordered_list = ordered_list[mid:]
+            # mid = mid + binary_seach(val, ordered_list[mid:])
+        else:
+            index += mid
+            break
     
-    return mid
+    return index
 
 
 # For testing purposes
 if __name__ == '__main__':
     test = [x for x in range(50)]
-    val = 34
-    index = binary_seach(val, test)
-    print(f'Index: {index}')
-    assert test[index] == val
+    for val in range(50):
+        index = binary_seach(val, test)
+        print(f'Index: {index}')
+        assert test[index] == val
